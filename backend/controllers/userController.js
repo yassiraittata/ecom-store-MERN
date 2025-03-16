4ff1ef67580facf7858585c2eb8f0d447241ba64
Yassir aittata <yassir.aittata@gmail.com>
2025-03-16 23:55:19 +0000

    validate data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,14 @@
 import asyncHandler from "express-async-handler";
+import { validationResult } from "express-validator";
+
 import User from "../models/userModel.js";
 
 export const createUser = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    throw new Error("Please enter valid data!");
+  }
   res.json({ message: "create user" });
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,19 @@
 import express from "express";
+import { body } from "express-validator";
+
 import { createUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser);
+router
+  .route("/")
+  .post(
+    [
+      body("username").isString(),
+      body("email").isString().isEmail(),
+      body("password").isString(),
+    ],
+    createUser
+  );
 
 export default router;
