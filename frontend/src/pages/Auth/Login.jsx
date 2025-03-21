d3d435ffb2d0439ad24916386de62e570ca6fe3a
Yassir aittata <yassir.aittata@gmail.com>
2025-03-21 00:00:05 +0000

    create login successfully

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -26,11 +26,31 @@ export default function Login() {
     }
   }, [navigate, redirect, userInfo]);
 
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    try {
+      const res = await logingApi({ email, password }).unwrap();
+
+      console.log(res);
+      dispatch(
+        setCredentials({
+          ...res.user,
+        })
+      );
+    } catch (error) {
+      console.log(error);
+      toast.error(
+        error?.data?.message || error?.message || "Something went wrong!"
+      );
+    }
+  }
+
   return (
-    <section className="pl-[10rem] flex flex-wrap">
+    <section className="pl-[10rem] flex flex-wrap  items-center justify-center">
       <div className="mr-[4rem] mt-[5rem]">
-        <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
-        <form className="container w-[40rem]">
+        <h1 className="text-3xl font-semibold mb-4">Sign In</h1>
+        <form className="container w-[40rem]" onSubmit={submitHandler}>
           <div className="my-[2rem]">
             <label
               htmlFor="email"
@@ -64,7 +84,25 @@ export default function Login() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center cursor-pointer"
+            disabled={isLaoding}
+          >
+            {isLaoding ? "Siging in ..." : "Sign in"}
+          </button>
         </form>
+        <div className="mt-4 text-sm">
+          <p className=" text-gray-700">
+            New customer?{" "}
+            <Link
+              className="text-blue-500 underline ml-1"
+              to={redirect ? `/register?redirect=${redirect}` : "/register"}
+            >
+              Create an account.
+            </Link>
+          </p>
+        </div>
       </div>
     </section>
   );
