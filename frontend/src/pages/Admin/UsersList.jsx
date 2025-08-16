c15f4684f6a348f79eaa4bd7b492de301e708262
Yassir aittata <yassir.aittata@gmail.com>
2025-08-16 15:25:18 +0100

    get user data

--- frontend/src/pages/Admin/UsersList.jsx
+++ frontend/src/pages/Admin/UsersList.jsx
@@ -9,6 +9,8 @@ import {
   useGetUserDetailsQuery,
   useUpdateUserMutation,
 } from "../../store/api/usersApiSlice";
+import Loader from "../../components/Loader";
+import Message from "../../components/Message";
 
 export default function UsersList() {
   const {
@@ -18,11 +20,88 @@ export default function UsersList() {
     isError,
     error,
   } = useGetAllUsersQuery();
+
   const [deleteUser] = useDeleteUserMutation();
 
   const [editableUserId, setEditableUserId] = useState(null);
   const [editableUserName, setEditableUserName] = useState(null);
   const [editableUserEmail, setEditableUserEmail] = useState(null);
 
-  return <div className="bg-red-600">usersList</div>;
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  function updateHandler(id) {}
+
+  function toggleEdit(id, name, email) {}
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4 ">Users</h1>
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Message variant="danger">
+          {error?.data.message || error.message}
+        </Message>
+      ) : (
+        <div className="flex flex-col md:flex-row">
+          <table className="w-full md::w-4/5 mx-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 text-left">ID</th>
+                <th className="px-4 py-2 text-left">Name</th>
+                <th className="px-4 py-2 text-left">Email</th>
+                <th className="px-4 py-2 text-left">Admin</th>
+                <th className="px-4 py-2 text-left"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => {
+                return (
+                  <tr key={user._id}>
+                    <td className="px-4 py-2">{user._id}</td>
+                    <td className="px-4 py-2">
+                      {editableUserId === user._id ? (
+                        <div className="flex items-center">
+                          <input
+                            type="text"
+                            value={editableUserName}
+                            onChange={(e) =>
+                              setEditableUserName(e.value.target)
+                            }
+                            className="w-full rounded-lg"
+                          />
+                          <button
+                            onClick={() => updateHandler(user._id)}
+                            className="ml-2 bg-blue-500 py-2 px-4 rounded-lg"
+                          >
+                            <FaCheck />
+                          </button>
+                        </div>
+                      ) : (
+                        <>
+                          <div className=" flex items-center">
+                            {user.username}{" "}
+                            <button
+                              className="ml-[1rem]"
+                              onClick={() =>
+                                toggleEdit(user._id, user.username, user.email)
+                              }
+                            >
+                              <FaEdit />
+                            </button>
+                          </div>
+                        </>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
