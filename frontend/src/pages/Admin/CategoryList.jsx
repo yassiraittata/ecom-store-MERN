5a1803479470f625fde228608ce4fe2370785454
Yassir aittata <yassir.aittata@gmail.com>
2025-08-18 22:48:25 +0100

    add loader

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -8,9 +8,10 @@ import {
 } from "../../store/api/categorySlice.js";
 import CategoryForm from "../../components/CategoryForm.jsx";
 import Modal from "../../components/Modal.jsx";
+import Loader from "../../components/Loader.jsx";
 
 const CategoryList = () => {
-  const { data: categories, refetch } = useGetAllCategoriesQuery();
+  const { data: categories, refetch, isLoading } = useGetAllCategoriesQuery();
   const [createCategory] = useCreateCategotyMutation();
   const [updateCategory] = useUpdateCategoryMutation();
 
@@ -90,24 +91,30 @@ const CategoryList = () => {
             />
             <br />
             <hr />
-            <div className="flex flex-wrap">
-              {categories?.map((category) => (
-                <div key={category._id}>
-                  <button
-                    className="bg-white border border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none foucs:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
-                    onClick={() => {
-                      {
-                        setModalIsVisible(true);
-                        setSelectedCategory(category);
-                        setUpdatedName(category.name);
-                      }
-                    }}
-                  >
-                    {category.name}
-                  </button>
-                </div>
-              ))}
-            </div>
+            {isLoading ? (
+              <div className="flex items-center justify-center p-4 w-full">
+                <Loader />
+              </div>
+            ) : (
+              <div className="flex flex-wrap">
+                {categories?.map((category) => (
+                  <div key={category._id}>
+                    <button
+                      className="bg-white border border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none foucs:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
+                      onClick={() => {
+                        {
+                          setModalIsVisible(true);
+                          setSelectedCategory(category);
+                          setUpdatedName(category.name);
+                        }
+                      }}
+                    >
+                      {category.name}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
             <Modal
               isOpen={modalIsVisible}
               onClose={() => setModalIsVisible(false)}
