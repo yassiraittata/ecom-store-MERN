fe02fd45ce88423d4c6ed15379e97891654ea860
Yassir aittata <yassir.aittata@gmail.com>
2025-08-20 14:45:26 +0100

    update product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ import { matchedData, validationResult } from "express-validator";
 import Product from "../models/productModel.js";
 
 export const createProduct = asyncHandler(async (req, res) => {
-  // const errors = validationResult(req);
+  const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).json({
@@ -12,7 +12,7 @@ export const createProduct = asyncHandler(async (req, res) => {
     });
   }
 
-  const { name, price, category, description, quantity, brand } =
+  const { name, price, category, description, quantity, brand, countInStock } =
     matchedData(req);
 
   const existedProduct = await Product.findOne({ name });
@@ -29,6 +29,7 @@ export const createProduct = asyncHandler(async (req, res) => {
     description,
     quantity,
     brand,
+    countInStock,
   });
 
   await product.save();
@@ -37,5 +38,42 @@ export const createProduct = asyncHandler(async (req, res) => {
     message: "Product created successfully",
     status: res.statusCode,
     product,
+  });
+});
+
+export const updateProduct = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array().map((err) => err.msg),
+      details: errors.array(), // full details if you want
+    });
+  }
+
+  const { name, price, category, description, quantity, brand, countInStock } =
+    matchedData(req);
+  const { id } = req.params;
+
+  const existedProduct = await Product.findById(id);
+
+  if (!existedProduct) {
+    res.status(404);
+    throw new Error("Product not found!");
+  }
+
+  existedProduct.name = name || existedProduct.name;
+  existedProduct.price = price || existedProduct.price;
+  existedProduct.category = category || existedProduct.category;
+  existedProduct.description = description || existedProduct.description;
+  existedProduct.quantity = quantity || existedProduct.quantity;
+  existedProduct.brand = brand || existedProduct.brand;
+  existedProduct.countInStock = countInStock || existedProduct.countInStock;
+  await existedProduct.save();
+
+  res.status(200).json({
+    message: "Product updated successfully",
+    status: res.statusCode,
+    product: existedProduct,
   });
 });
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,7 +7,10 @@ import { checkId } from "../middlewares/checkId.js";
 
 const routes = express.Router();
 
-import { createProduct } from "../controllers/productController.js";
+import {
+  createProduct,
+  updateProduct,
+} from "../controllers/productController.js";
 
 routes
   .route("/")
@@ -31,6 +34,30 @@ routes
         .withMessage("Description must be a string"),
     ],
     createProduct
+  );
+
+routes
+  .route("/:id")
+  .put(
+    authenticate,
+    isAdmin,
+    formidable(),
+    [
+      body("name").notEmpty().withMessage("Name is required"),
+      body("price").isNumeric().withMessage("Price must be a number"),
+      body("brand").notEmpty().withMessage("Brand is required"),
+      body("image").notEmpty().withMessage("Brand is required"),
+      body("quantity").isNumeric().withMessage("Quantity must be a number"),
+      body("category").notEmpty().withMessage("Category is required"),
+      body("countInStock")
+        .isNumeric()
+        .withMessage("Count in stock must be a number"),
+      body("description")
+        .optional()
+        .isString()
+        .withMessage("Description must be a string"),
+    ],
+    updateProduct
   );
 
 export default routes;
